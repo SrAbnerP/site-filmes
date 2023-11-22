81cfa9d9eb33e4b715af142176b655ded9899ad1
SrAbnerP <abner.sol.pereira@gmail.com>
2023-11-22 18:14:22 -0300

    creating the projetct

---
+++ site-filmes/src/main/java/com/project/sitefilmes/SiteFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.project.sitefilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteFilmesApplication.class, args);
+	}
+
+}
---
+++ site-filmes/src/test/java/com/project/sitefilmes/SiteFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sitefilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
